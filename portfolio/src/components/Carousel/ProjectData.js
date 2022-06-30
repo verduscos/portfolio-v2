21242f398e56f69821a10a348def9945cbb226e1
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-29 17:12:20 -0700

    added porject images

--- portfolio/src/components/Carousel/ProjectData.js
+++ portfolio/src/components/Carousel/ProjectData.js
@@ -1,27 +1,38 @@
+import pf from "../../assets/images/splash-pf.PNG";
+import nk from "../../assets/images/splash-nk.PNG";
+import qwerty from "../../assets/images/splash-qwerty.PNG";
+import jj from "../../assets/images/splash-jj.PNG";
+import kotrr from "../../assets/images/splash-kotrr.PNG"
 const ProjectData = [
   {
     title: "Paranormal Files",
-    image: "https://picsum.photos/200/300",
+    image: pf,
     link: "https://paranormalfiles.herokuapp.com/",
     description: "Built with React/Redux for the frontend and Python/Flask for the backend with a PostgreSQL database. Paranormal Files, loosely based on Medium, is a social platform where users can share or read about experiences with the paranormal."
   },
   {
     title: "NoteKeeper",
-    image: "https://picsum.photos/200/301",
+    image: nk,
     link: "https://lista-nota.herokuapp.com/",
     description: "NoteKeeper is a note-taking app that features notes and notebooks for easier organization. Users can store notes in specific notebooks to better organize their ideas/plans or whatever they don't want to forget. This app is modeled after the popular note-taking web app Evernote."
   },
   {
     title: "Qwerty",
-    image: "https://picsum.photos/200/302",
+    image: qwerty,
     link: "https://qwerty2022.herokuapp.com/",
     description: "Qwerty, a full-stack application inspired by Etsy, is an e-commerce platform for keyboard enthusiasts to buy and sell mechanical keyboard components."
   },
   {
     title: "jollyJingles",
-    image: "https://picsum.photos/200/302",
+    image: jj,
     link: "https://jolly-jingles.herokuapp.com/",
     description: "jollyJingles, a full-stack application inspired by Goodreads, is an social platform for music enthusiasts to share and review jollyJingles."
+  },
+  {
+    title: "KOTRR",
+    image: kotrr,
+    link: "https://verduscos.github.io/KOTR-Reviews/",
+    description: "Prior to joining App Academy, I developed a static website for video game reviews. This project holds a special place in my heart since it's the first freelance project I ever worked on. Created using HTML, CSS, and Javascript."
   }
 ];
 
