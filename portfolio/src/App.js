7e681d6139f1e7f2070d9b4b2215102cf945832a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-29 21:07:59 -0700

    updated App with react v6 syntax

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,29 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
 import Projects from './components/Carousel';
 import './App.css';
+
+let home = (
+  <>
+    <Nav />
+    <Projects />
+  </>
+)
+
+let about = (
+  <>
+    <Nav />
+  </>
+)
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' exact={true} element={<Projects />} />
+        <Route exact path='/' element={home} />
+        <Route path='about' element={about} />
+        <Route path='projects' element={about} />
+        <Route path='contact' element={about} />
       </Routes>
     </BrowserRouter>
   );
---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,26 @@
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = () => {
+
+  return (
+    <nav id="nav">
+      <ul>
+      <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/contact">Contact</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav;
