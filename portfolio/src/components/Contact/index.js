71c339389e18d3c5f8365e33141f16bed0ab6ef6
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-06-30 17:08:13 -0700

    completed contact section

--- portfolio/src/components/Contact/index.js
+++ portfolio/src/components/Contact/index.js
@@ -1,10 +1,38 @@
 import React from "react";
+import "./Contact.css"
 
 function Contact() {
 
   return (
-    <div>
-      <h1>Contact</h1>
+    <div id="contact">
+      <h1>Hi there 👋</h1>
+      <ul id="contact-inner">
+        <li>
+          <p>
+            I'm Eddie, a full stack web developer passionate about development and someone who values the visual feedback of creating clean, usable products.</p>
+          <p>Let's chat! How to reach me:</p>
+        </li>
+        <li id="contact-email">
+          <a href="mailto:eduardo.verdusco@yahoo.com">
+            eduardo.verdusco@yahoo.com
+          </a>
+        </li>
+        <li>
+          <a href="tel:7024266788" target='_blank'>
+            702-426-6788
+          </a>
+        </li>
+        {/* <li>
+          <a href="https://github.com/verduscos" target='_blank'>
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/eddie-verdusco/" target='_blank'>
+            LinkedIn
+          </a>
+        </li> */}
+      </ul>
     </div>
   )
 }
