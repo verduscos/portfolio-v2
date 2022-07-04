7ce9733e827747d573a78c0d663ce70946ae3aed
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-03 22:12:04 -0700

    updated about title

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Self from "../../assets/images/personal.jpg";
 import "./About.css";
 
@@ -7,7 +6,7 @@ function About() {
 
   return (
     <div id="about">
-        <h1>My journey</h1>
+      <h1 id="about-title">My journey</h1>
       <div id="bio">
         <p>My love for anything tech-related started at a very young age. I never realized that solving puzzles as my childhood hero, Link, from The Legend of Zelda, would spark my curiosity and instill me with a love for problem-solving. My heart was always on joining the tech industry, but life led me into sales first. While in car sales, I developed skills in managing deadlines, prioritizing tasks, and being able to work with people from any walk of life. I learned the value of being a self-starter, the importance of self-reliance, and when to ask for help or when to "turn the deal" in car lingo. It’s my unique trajectory that will set me apart from other developers.</p>
         <img id="me" src={Self} alt="Eddie V" />
