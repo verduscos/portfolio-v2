79a00418cf1e41cc2c15b8a0aa48de4af71fda08
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-03 22:21:35 -0700

    updated ProjectData

--- portfolio/src/components/ProjectData.js
+++ portfolio/src/components/ProjectData.js
@@ -10,6 +10,7 @@ const ProjectData = [
     title: "Paranormal Files",
     image: pf,
     link: "https://paranormalfiles.herokuapp.com/",
+    repo: "https://github.com/verduscos/medium-clone",
     description: "Built with React/Redux for the frontend and Python/Flask for the backend with a PostgreSQL database. Paranormal Files, loosely based on Medium, is a social platform where users can share or read about experiences with the paranormal."
   },
   {
@@ -17,6 +18,7 @@ const ProjectData = [
     title: "NoteKeeper",
     image: nk,
     link: "https://lista-nota.herokuapp.com/",
+    repo: "https://github.com/verduscos/NoteKeeper",
     description: "NoteKeeper is a note-taking app that features notes and notebooks for easier organization. Users can store notes in specific notebooks to better organize their ideas/plans or whatever they don't want to forget. This app is modeled after the popular note-taking web app Evernote."
   },
   {
@@ -24,6 +26,7 @@ const ProjectData = [
     title: "Qwerty",
     image: qwerty,
     link: "https://qwerty2022.herokuapp.com/",
+    repo: "https://github.com/verduscos/etsy-group-project",
     description: "Qwerty, a full-stack application inspired by Etsy, is an e-commerce platform for keyboard enthusiasts to buy and sell mechanical keyboard components."
   },
   {
@@ -31,6 +34,7 @@ const ProjectData = [
     title: "jollyJingles",
     image: jj,
     link: "https://jolly-jingles.herokuapp.com/",
+    repo: "https://github.com/verduscos/Jolly-Jingles",
     description: "jollyJingles, a full-stack application inspired by Goodreads, is an social platform for music enthusiasts to share and review jollyJingles."
   },
   {
@@ -38,6 +42,7 @@ const ProjectData = [
     title: "KOTRR",
     image: kotrr,
     link: "https://verduscos.github.io/KOTR-Reviews/",
+    repo: "https://github.com/verduscos/KOTR-Reviews",
     description: "Prior to joining App Academy, I developed a static website for video game reviews. This project holds a special place in my heart since it's the first freelance project I ever worked on. Created using HTML, CSS, and Javascript."
   }
 ];
