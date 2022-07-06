2f5cec7458fc0ea9d4915770c874b2c50ab70519
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-06 12:08:34 -0700

    updating carousel

--- portfolio/src/components/Carousel/index.js
+++ portfolio/src/components/Carousel/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoIosRadioButtonOff, IoIosRadioButtonOn } from "react-icons/io"
+import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 import ProjectData from "../ProjectData";
 import "./Carousel.css"
 
@@ -9,25 +10,30 @@ function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   let currentProject = ProjectData[currentIndex];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (currentIndex > ProjectData.length - 2) {
-        setCurrentIndex(0);
-      } else {
-        setCurrentIndex(currentIndex + 1);
-      }
-    }, 3500);
+  let projects = ProjectData.filter((project) => project.id !== currentIndex);
 
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    }
-  })
+  console.log(currentIndex)
+  console.log(projects)
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     if (currentIndex > ProjectData.length - 2) {
+  //       setCurrentIndex(0);
+  //     } else {
+  //       setCurrentIndex(currentIndex + 1);
+  //     }
+  //   }, 3500);
+
+  //   return () => {
+  //     if (interval) {
+  //       clearInterval(interval);
+  //     }
+  //   }
+  // })
 
   return (
     <div className="carousel">
-      <div className="inner">
+      {/* <div className="inner">
         <h1 id="carousel-title">Projects</h1>
         <Link to={`/project/${currentProject.id}`}>
           <div id="overlay">
@@ -35,11 +41,11 @@ function Carousel() {
           </div>
           <img className="carousel-image" src={currentProject.image} alt="project image" />
         </Link>
-      </div>
+      </div> */}
 
       <ul className="project-btns">
         {indexes.map((index) => (
-          <li key={index} value={index} onClick={(e) => setCurrentIndex(index)}>
+          <li key={index} value={index} onMouseOver={(e) => setCurrentIndex(index)}>
             {
               currentIndex === index ?
                 <IoIosRadioButtonOn size={30} /> :
@@ -47,6 +53,20 @@ function Carousel() {
             }
           </li>
         ))}
+      </ul>
+
+      {currentIndex}
+
+      <div>testing down here, okay</div>
+      <ul id="new-carousel">
+
+        {projects.map(project => (
+
+// project-${project.id}
+          <img className={`test-img`} src={project.image} />
+        ))}
+
+        <img className={`test-img project-${currentProject.id}`} src={currentProject.image} />
       </ul>
     </div>
   );
