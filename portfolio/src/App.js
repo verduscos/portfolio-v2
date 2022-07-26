3be4667c4ea26898c796fa7e65660f75040c068d
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-26 16:31:43 -0700

    added basename to entry file

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='about' element={<Main />} />
