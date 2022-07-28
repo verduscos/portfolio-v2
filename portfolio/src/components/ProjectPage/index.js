159e43f8a3ef4c216d6a720e1d4b45172df8d62e
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2022-07-27 21:30:02 -0700

    added back link to project page

--- portfolio/src/components/ProjectPage/index.js
+++ portfolio/src/components/ProjectPage/index.js
@@ -1,4 +1,6 @@
 import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import { MdKeyboardBackspace } from "react-icons/md";
 import ProjectData from "../ProjectData";
 import "./ProjectPage.css";
 
@@ -10,14 +12,15 @@ function ProjectPage() {
   return (
     <div id="ProjectPage">
       <ul id="project-page-inner">
-        <li>
-          <h1 id="project-page-title">{project?.title}</h1>
+        <li id="project-header">
+          <h1 id="project-page-title" className="project-header-tag">{project?.title}</h1>
+          <Link to="/" className="project-header-tag"><MdKeyboardBackspace /></Link>
         </li>
         <li>
           <img id="project-page-image" src={project?.image} />
         </li>
         <li id="project-links-container">
-          <p><b>Tech:</b> {project?.stack}`</p>
+          <p><b>Tech:</b> {project?.stack}</p>
           <div>
             <b>
               <a className="project-link" href={project?.link} target="_blank">Live</a>
